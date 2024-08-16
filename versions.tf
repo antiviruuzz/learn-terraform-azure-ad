ca665d7c9828e7c13abb9c279b57dfbc039419f0
antiviruuzz <antiviruuz@gmail.com>
2024-08-16 16:22:10 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -5,7 +5,20 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.41.0"
+      version = ">= 0.7.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.67.0"
     }
   }
+
 }
+
+provider "azuread" {}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
